4fb218345659c15c3639ab19dd958eeb36238771
husky <1101648204@qq.com>
2019-12-10 20:35:48 +0800

    更新

--- src/main/java/com/zzx/mongodb/service/delect/DeleteFromMongodb.java
+++ src/main/java/com/zzx/mongodb/service/delect/DeleteFromMongodb.java
@@ -8,7 +8,7 @@ import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
-@Service
+@Service("deleteFromMongodb")
 public class DeleteFromMongodb {
     @Autowired
     private MongoTemplate mongoTemplate;
