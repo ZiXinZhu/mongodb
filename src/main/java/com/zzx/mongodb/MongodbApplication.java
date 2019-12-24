6a9c2d7a8635c003fbcf04765adf84aa519d2f75
husky <1101648204@qq.com>
2019-12-24 21:19:29 +0800

    Husky:add logs

--- src/main/java/com/zzx/mongodb/MongodbApplication.java
+++ src/main/java/com/zzx/mongodb/MongodbApplication.java
@@ -1,15 +1,12 @@
 package com.zzx.mongodb;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 public class MongodbApplication {
 
     public static void main(String[] args) {
-        log.info("项目开始启动！");
         SpringApplication.run(MongodbApplication.class, args);
     }
 
