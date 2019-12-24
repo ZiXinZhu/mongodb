2877340a85cf5a628076701acac278d6f2ac44c7
husky <1101648204@qq.com>
2019-12-24 21:09:29 +0800

    Husky:add logs

---
+++ src/main/java/com/zzx/mongodb/MongodbApplication.java
@@ -0,0 +1,17 @@
+package com.zzx.mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Import;
+
+
+//@EnableAspectJAutoProxy
+@SpringBootApplication
+public class MongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongodbApplication.class, args);
+    }
+
+}
