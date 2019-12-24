b1d4cf8b36828754db70aad1415d6f74e660dafb
Husky <1101648204@qq.com>
2019-12-24 21:18:51 +0800

    Update MongodbApplication.java

--- src/main/java/com/zzx/mongodb/MongodbApplication.java
+++ src/main/java/com/zzx/mongodb/MongodbApplication.java
@@ -3,11 +3,8 @@ package com.zzx.mongodb;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
 
-@Slf4j
-@EnableAspectJAutoProxy
 @SpringBootApplication
 public class MongodbApplication {
 
